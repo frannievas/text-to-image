b24be2e526d389c9f343e327c84df588fbd7c12d
Francisco Nievas <frannievas@gmail.com>
2019-01-09 18:29:18 -0300

    Add data loader for cub dataset. Use path lib instead of str

--- data_loader.py
+++ data_loader.py
@@ -1,4 +1,3 @@
-
 import os
 import re
 import time
@@ -7,11 +6,13 @@ import re
 import string
 import tensorlayer as tl
 from utils import *
+from pathlib import Path
 
+# Set option for wich dataset want to train
+dataset = 'cub' #
 
-dataset = '102flowers' #
 need_256 = True # set to True for stackGAN
-datapath = "~/resources/102flowers/"
+datapath = str(Path.home()) + '/resources/'
 
 
 if dataset == '102flowers':
@@ -20,7 +21,7 @@ if dataset == '102flowers':
     captions_ids = [80000, any]
     """
     # cwd = os.getcwd()
-    cwd = datapath
+    cwd = datapath + '102flowers/'
     img_dir = os.path.join(cwd, '102flowers')
     caption_dir = os.path.join(cwd, 'text_c10')
     VOC_FIR = cwd + '/vocab.txt'
@@ -158,6 +159,117 @@ if dataset == '102flowers':
     # save_images(b_images, [8, 8], 'temp2.png')
     # # tl.visualize.images2d(b_images, second=5, saveable=True, name='temp2')
     # exit()
+
+if dataset == 'cub':
+    """
+    images.shape = [8000, 64, 64, 3]
+    captions_ids = [80000, any]
+    """
+    cwd = datapath + 'cub2002011/'
+    img_dir = os.path.join(cwd, 'cub2002011')
+    caption_dir = os.path.join(cwd, 'text_c10')
+    VOC_FIR = cwd + '/vocab.txt'
+
+    ## load captions
+    caption_sub_dir = load_folder_list( caption_dir )
+    captions_dict = {}
+    processed_capts = []
+    for sub_dir in caption_sub_dir: # get caption file list
+        with tl.ops.suppress_stdout():
+            files = tl.files.load_file_list(path=sub_dir, regx='.txt')
+            for i, f in enumerate(files):
+                file_dir = os.path.join(sub_dir, f)
+                key = int(re.findall('\d+', f)[0])
+                t = open(file_dir,'r')
+                lines = []
+                for line in t:
+                    line = preprocess_caption(line)
+                    lines.append(line)
+                    processed_capts.append(tl.nlp.process_sentence(line, start_word="<S>", end_word="</S>"))
+                assert len(lines) == 10, "Every flower image have 10 captions"
+                captions_dict[key] = lines
+    print(" * %d x %d captions found " % (len(captions_dict), len(lines)))
+
+    ## build vocab
+    if not os.path.isfile('vocab.txt'):
+        _ = tl.nlp.create_vocab(processed_capts, word_counts_output_file=VOC_FIR, min_word_count=1)
+    else:
+        print("WARNING: vocab.txt already exists")
+    vocab = tl.nlp.Vocabulary(VOC_FIR, start_word="<S>", end_word="</S>", unk_word="<UNK>")
+
+    ## store all captions ids in list
+    captions_ids = []
+    try: # python3
+        tmp = captions_dict.items()
+    except: # python3
+        tmp = captions_dict.iteritems()
+    for key, value in tmp:
+        for v in value:
+            captions_ids.append( [vocab.word_to_id(word) for word in nltk.tokenize.word_tokenize(v)] + [vocab.end_id])  # add END_ID
+            # print(v)              # prominent purple stigma,petals are white inc olor
+            # print(captions_ids)   # [[152, 19, 33, 15, 3, 8, 14, 719, 723]]
+            # exit()
+    captions_ids = np.asarray(captions_ids)
+    print(" * tokenized %d captions" % len(captions_ids))
+
+    ## check
+    img_capt = captions_dict[1][1]
+    print("img_capt: %s" % img_capt)
+    print("nltk.tokenize.word_tokenize(img_capt): %s" % nltk.tokenize.word_tokenize(img_capt))
+    img_capt_ids = [vocab.word_to_id(word) for word in nltk.tokenize.word_tokenize(img_capt)]#img_capt.split(' ')]
+    print("img_capt_ids: %s" % img_capt_ids)
+    print("id_to_word: %s" % [vocab.id_to_word(id) for id in img_capt_ids])
+
+    ## load images
+    with tl.ops.suppress_stdout():  # get image files list
+        # Cub dataset is ordered in sub folders so retrieve images for each
+        # sub folder
+        imgs_title_list = []
+        for sub_dir in load_folder_list(img_dir):
+            imgs_title_list += [os.path.join(sub_dir, img)
+                for img in sorted(tl.files.load_file_list(path = sub_dir, regx = '.jpg'))]
+
+    print(" * %d images found, start loading and resizing ..." % len(imgs_title_list))
+    s = time.time()
+    images = []
+    images_256 = []
+    try:
+        for name in imgs_title_list:
+            # print(name)
+            img_raw = scipy.misc.imread( os.path.join(img_dir, name) )
+            # Image is black and white, convert to "rgb"
+            if img_raw.ndim == 2:
+                img_raw = np.stack((img_raw,)*3, axis=-1)
+            img = tl.prepro.imresize(img_raw, size=[64, 64])    # (64, 64, 3)
+            img = img.astype(np.float32)
+            images.append(img)
+            if need_256:
+                img = tl.prepro.imresize(img_raw, size=[256, 256]) # (256, 256, 3)
+                img = img.astype(np.float32)
+
+                images_256.append(img)
+    except:
+        import ipdb; ipdb.set_trace()
+    # images = np.array(images)
+    # images_256 = np.array(images_256)
+    print(" * loading and resizing took %ss" % (time.time()-s))
+    n_images = len(captions_dict)
+    n_captions = len(captions_ids)
+    n_captions_per_image = len(lines) # 10
+
+    print("n_captions: %d n_images: %d n_captions_per_image: %d" % (n_captions, n_images, n_captions_per_image))
+
+    captions_ids_train, captions_ids_test = captions_ids[: 8000*n_captions_per_image], captions_ids[8000*n_captions_per_image :]
+    images_train, images_test = images[:8000], images[8000:]
+    if need_256:
+        images_train_256, images_test_256 = images_256[:8000], images_256[8000:]
+    n_images_train = len(images_train)
+    n_images_test = len(images_test)
+    n_captions_train = len(captions_ids_train)
+    n_captions_test = len(captions_ids_test)
+    print("n_images_train:%d n_captions_train:%d" % (n_images_train, n_captions_train))
+    print("n_images_test:%d  n_captions_test:%d" % (n_images_test, n_captions_test))
+
 
 import pickle
 def save_all(targets, file):
